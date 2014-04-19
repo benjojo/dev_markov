b03518ea852e33186fec8a50f47c5505eaa53f41
Ben Cox <ben@benjojo.co.uk>
2014-04-19 16:55:55 +0100

    Fixed spelling error that broke build

--- devMkov.c
+++ devMkov.c
@@ -12,6 +12,6 @@ MODULE_AUTHOR("Ben Cartwright-Cox");
 
 static int dev_open(struct inode *, struct file *);
 static int dev_rls(struct inode *, struct file *);
-static ssize_t dev_read(strict file *, char *, size_t, loff_t *);
-static ssize_t dev_write(strict file *, const char *, size_t, loff_t *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
