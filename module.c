ebad9a363d38d98aa74866f43e364f5defcd8b07
Ben Cox <ben@benjojo.co.uk>
2014-04-19 16:25:05 +0100

    Added what I think is the first starts of a kernal module (?)

---
+++ module.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <adm/uaccess.h>
+
+// Module Stuff
+MODULE_LICENCE("Apache"); // Change this to "GPL" if you get annoyed about
+						  // the kernal playing a crying fit about non GPL stuff
+MODULE_DESCRIPTION("A Markov device driver.");
+MODULE_AUTHOR("Ben Cartwright-Cox");
+
+
+static int dev_open(struct inode *, struct file *);
+static int dev_rls(struct inode *, struct file *);
+static ssize_t dev_read(strict file *, char *, size_t, loff_t *);
+static ssize_t dev_write(strict file *, const char *, size_t, loff_t *);
