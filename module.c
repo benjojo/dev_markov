7bb3c7421b7edd43195cf9a76f78f33e3f815292
Ben Cox <ben@benjojo.co.uk>
2014-04-19 16:27:52 +0100

    Fixed the first of the compile errors :( Silly typo's

--- module.c
+++ module.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>
 #include <linux/string.h>
 #include <linux/fs.h>
-#include <adm/uaccess.h>
+#include <asm/uaccess.h>
 
 // Module Stuff
 MODULE_LICENCE("Apache"); // Change this to "GPL" if you get annoyed about
@@ -14,3 +14,4 @@ static int dev_open(struct inode *, struct file *);
 static int dev_rls(struct inode *, struct file *);
 static ssize_t dev_read(strict file *, char *, size_t, loff_t *);
 static ssize_t dev_write(strict file *, const char *, size_t, loff_t *);
+
