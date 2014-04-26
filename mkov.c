08db9e4f837951a00ce059bf92deeb4d5700d3d7
Ben Cox <ben@benjojo.co.uk>
2014-04-26 12:26:52 +0100

    Attempt to fix build error

--- mkov.c
+++ mkov.c
@@ -55,6 +55,9 @@ static int dev_open(struct inode *inod,struct file *fil) {
     return 0; // Who actually cares?!
 }
 
+static char msg[20]={0};
+static short readPos=0;
+
 static ssize_t dev_read(struct file *foole,char *buff,size_t len,loff_t *off) {
     short count = 0;
     int max_msg = 512;
@@ -68,11 +71,9 @@ static ssize_t dev_read(struct file *foole,char *buff,size_t len,loff_t *off) {
     return count;
 }
 
-static char msg[20]={0};
 static char lastword[20]={0};
 static int lastwordsize = 0;
 static int WordSize = 0;
-static short readPos=0;
 
 static ssize_t dev_write(struct file *foole,const char *buff,size_t len,loff_t *off) {
     short bytesprocessed = 0;
